5527ac321466473fedde9fb56ae516f3963a860b
red6kirito <lafinfrt@gmail.com>
2024-06-01 19:22:52 +0200

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,58 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Mon super saiyan prefere ?",
+        answer: "C. super saiyan god",
+        options: [
+            "A. super saiyan 1",
+            "B. super saiyan blue",
+            "C. super saiyan god",
+            "D. super saiyan 2"
+        ]
+    },
+    {
+        numb: 2,
+        question: "qu'elle est le jeu ou j'ai le plus d'heure de jeu ?(1400 heures)",
+        answer: "B. Minecraft",
+        options: [
+            "A. Rainbow Six Siege",
+            "B. Minecraft",
+            "C. Call Of Duty",
+            "D. GTA V"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Mon anime prefere ?",
+        answer: "A. Dragon ball",
+        options: [
+            "A. Dragon ball",
+            "B. Sword Art Online",
+            "C. The Seven Deadly Sins",
+            "D. Naruto"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Mon perso prefere de dragon ball ?",
+        answer: "B. Bardock",
+        options: [
+            "A. Sangoku",
+            "B. Bardock",
+            "C. Vegeta",
+            "D. Gogeta"
+        ]
+    },
+
+    {
+        numb: 5,
+        question: "Ma passion semi-cachée ?",
+        answer: "D. L'écriture",
+        options: [
+            "A. Les Maths",
+            "B. Manger",
+            "C. Dessiner",
+            "D. L'écriture"
+        ]
+    },
+];
